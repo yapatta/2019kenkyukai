b716033ad5025107dc2ae9a862e07383e9f4d99d
Yujiro Yahata <syani4732@gmail.com>
2019-10-22 14:36:35 +0900

    compared to quick sort

--- radix-sort/lsd.cpp
+++ radix-sort/lsd.cpp
@@ -1,56 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
+using ll = long long;
+const ll MAX_N = 1e9;
+#define KEY 16
+ll vec[MAX_N];
 
-int getmax(const vector<int> &a, int n) {
-	int res = a[0];
-	for(int x=1;x<n;x++) {
+ll getmax(ll *a, ll n) {
+	ll res = a[0];
+	for(ll x=1;x<n;x++) {
 		res = (res < a[x]) ? a[x] : res;
 	}
 	return res;
 }
 
-void countsort(vector<int> &a, int n, int exp) {
-	int result[n], i, count[10] = {0};
+void countsort(ll *a, ll n, ll exp) {
+	ll result[n], i, count[KEY] = {0};
 
-	for(int i=0;i<n;i++) {
-		count[(a[i] / exp) % 10]++;	
+	for(ll i=0;i<n;i++) {
+		count[(a[i] / exp) % KEY]++;	
 	}
 
-	for(i=1;i<10;i++) {
+	for(i=1;i<KEY;i++) {
 		count[i] += count[i-1];
 	}
 
 	for(i=n-1;i>=0;i--) {
-		result[count[(a[i] / exp) % 10] - 1] = a[i];
-		count[(a[i] / exp) % 10]--;
+		result[count[(a[i] / exp) % KEY] - 1] = a[i];
+		count[(a[i] / exp) % KEY]--;
 	}
 	for(i=0;i<n;i++) {
 		a[i] = result[i];
 	}
 }
 
-void radix_sort(vector<int> &a, int n) {
-	int exp, max;
+void radix_sort(ll *a, ll n) {
+	ll exp, max;
 	max = getmax(a, n);
-	for(exp=1;max/exp>0;exp*=10) {
+	for(exp=1;max/exp>0;exp*=KEY) {
 		countsort(a , n , exp);
 	}
 }
 
 int main() {
-	int n;
+	ll n;
 	cout << "Input the number of elements to be sorted: ";
 	cin >> n;
-	vector<int> vec(n);
-	for(int i=0;i<n;i++) cin >> vec[i];
-	cout << "Before sorting" << endl;
-	for(int i=0;i<n;i++) {
-		printf("%d%c", vec[i], (i==n-1) ? '\n' : ' ');
+
+	random_device rnd;
+	mt19937 mt(rnd());
+
+	for(ll i=0;i<n;i++) vec[i] = mt();
+	cout << "Before sorting (quick sort)" << endl;
+	for(ll i=0;i<n;i++) {
+		printf("%lld%c", vec[i], (i==n-1) ? '\n' : ' ');
+	}
+	sort(vec, vec+n);
+	cout << "finished" << endl;
+	for(ll i=0;i<n;i++) {
+		printf("%lld%c", vec[i], (i==n-1) ? '\n' : ' ');
+	}
+
+	for(ll i=0;i<n;i++) vec[i] = mt();
+	cout << "Before sorting (lsd radix sort)" << endl;
+	for(ll i=0;i<n;i++) {
+		printf("%lld%c", vec[i], (i==n-1) ? '\n' : ' ');
 	}
 	radix_sort(vec, n);
-	cout << "After sorting" << endl;
-	for(int i=0;i<n;i++) {
-		printf("%d%c", vec[i], (i==n-1) ? '\n' : ' ');
+	cout << "finished" << endl;
+	for(ll i=0;i<n;i++) {
+		printf("%lld%c", vec[i], (i==n-1) ? '\n' : ' ');
 	}
 }
+
--- radix-sort/msd.cpp
+++ radix-sort/msd.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-const int MAX_N = 1e9;
+#include <random>
 using namespace std;
+using ll = long long;
+const ll MAX_N = 1e9;
 
-int vec[MAX_N];
+ll vec[MAX_N];
 
-int getmax(int *a, int *en, int n) {
-	int res = a[0];
-	for(int *ptr = a;ptr != en; ptr++) {
+ll getmax(ll *a, ll *en, ll n) {
+	ll res = a[0];
+	for(ll *ptr = a;ptr != en; ptr++) {
 		if(res < *ptr)
 			res = *ptr;
 	}
 	return res;
 }
 
-int getmaxradix(int key) {
-	int res = 1;
+ll getmaxradix(ll key) {
+	ll res = 1;
 	while(1) {
 		if(key / 10 == 0) {
 			break;
@@ -28,37 +30,37 @@ int getmaxradix(int key) {
 	return res;
 }
 
-void countsort(int *a, int *en, int n, int exp) {
+void countsort(ll *a, ll *en, ll n, ll exp) {
 	if(exp <= 0 || n <= 1) return;
-	int result[n], counter[11] = {0}, counter_cpy[11] = {0};
-	for(int i=0;i<n;i++) {
+	ll result[n], counter[11] = {0}, counter_cpy[11] = {0};
+	for(ll i=0;i<n;i++) {
 		counter[(a[i]/exp)%10]++;
 	}
 
-	vector<int> wid;
-	for(int i=0;i<10;i++) {
+	vector<ll> wid;
+	for(ll i=0;i<10;i++) {
 		if(counter[i] != counter[i+1])
 			wid.push_back(i);
 	}
-	for(int i=1;i<10;i++) {
+	for(ll i=1;i<10;i++) {
 		counter[i] += counter[i-1];
 	}
-	for(int i=0;i<10;i++) {
+	for(ll i=0;i<10;i++) {
 		counter_cpy[i] = counter[i];
 	}
 
-	for(int i=n-1;i>=0;i--) {
+	for(ll i=n-1;i>=0;i--) {
 		result[counter[(a[i]/exp)%10] - 1] = a[i];
 		counter[(a[i]/exp)%10]--;
 	}
 
-	for(int i=0;i<n;i++) {
+	for(ll i=0;i<n;i++) {
 		a[i] = result[i];
 	}
 	
-	int before = 0;
-	int now = counter_cpy[wid[0]];
-	for(int i=0;i<wid.size();i++) {
+	ll before = 0;
+	ll now = counter_cpy[wid[0]];
+	for(ll i=0;i<wid.size();i++) {
 		countsort(a+before, a+now, now - before, exp/10);
 		if(i+1==wid.size()) break;
 		before = counter_cpy[wid[i]];
@@ -66,24 +68,39 @@ void countsort(int *a, int *en, int n, int exp) {
 	}
 }
 
-void radix_sort(int *a, int *en, int n) {
-	int max = getmax(a, en, n);
-	int maxradix = getmaxradix(max);
+void radix_sort(ll *a, ll *en, ll n) {
+	ll max = getmax(a, en, n);
+	ll maxradix = getmaxradix(max);
 	countsort(a, en, n, pow(10, maxradix-1));
 }
 
 int main() {
-	int n;
+	ll n;
 	cout << "Input the number of elements to be sorted: ";
 	cin >> n;
-	for(int i=0;i<n;i++) cin >> vec[i];
-	cout << "Before sorting" << endl;
-	for(int i=0;i<n;i++) {
-		printf("%d%c", vec[i], (i==n-1) ? '\n' : ' ');
+	
+	random_device rnd;
+	mt19937 mt(rnd());
+	for(ll i=0;i<n;i++) vec[i] = mt();
+	cout << "Before sorting (msd radix sort)" << endl;
+	/*
+	for(ll i=0;i<n;i++) {
+		printf("%lld%c", vec[i], (i==n-1) ? '\n' : ' ');
 	}
+	*/
+
 	radix_sort(vec, vec+n, n);
+	cout << "finished" << endl;
+
+	for(ll i=0;i<n;i++) vec[i] = mt();
+	cout << "Before sorting (quick sort)" << endl;
+	sort(vec, vec+n);
+	cout << "finished" << endl;
+
+	/*
 	cout << "After sorting" << endl;
-	for(int i=0;i<n;i++) {
-		printf("%d%c", vec[i], (i==n-1) ? '\n' : ' ');
+	for(ll i=0;i<n;i++) {
+		printf("%lld%c", vec[i], (i==n-1) ? '\n' : ' ');
 	}
+	*/
 }
